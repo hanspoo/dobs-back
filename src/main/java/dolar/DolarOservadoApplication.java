cb708c87b44ec5a54ff8ff3ca58ab6f93e9114dc
Hans Poo <hans@welinux.cl>
2019-12-29 08:30:32 -0300

    Conecta a base de datos

---
+++ src/main/java/dolar/DolarOservadoApplication.java
@@ -0,0 +1,13 @@
+package dolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolarOservadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolarOservadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dolar/DolarOservadoApplicationTests.java
@@ -0,0 +1,13 @@
+package dolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolarOservadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
